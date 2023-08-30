0b1b2972eedd631ff08507338ec66259c8a47de1
AlfredVilorio <25935213-AlfredVilorio@users.noreply.replit.com>
2023-08-30 21:31:56 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,26 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+   
+    Console.WriteLine ("Lucinda Potter");
+    Console.WriteLine ("6/24/1992");
+    Console.WriteLine ("Work 000-000-0101");
+    Console.WriteLine ("Cell 000-000-0189");
+
+    Console.WriteLine ("John Smith");
+    Console.WriteLine ("8/15/1978");
+    Console.WriteLine ("Work 000-111-0101");
+    Console.WriteLine ("Cell 000-111-0189");
+    
+    Console.WriteLine ("Chris Johnson");
+    Console.WriteLine ("12/01/1987");
+    Console.WriteLine ("Work 000-222-0101");
+    Console.WriteLine ("Cell 000-222-0189");
+
+    Console.WriteLine ("Robert Hall");
+    Console.WriteLine ("2/27/1949");
+    Console.WriteLine ("Work 000-444-0101");
+    Console.WriteLine ("Cell 000-444-0189");
+  }
+}
